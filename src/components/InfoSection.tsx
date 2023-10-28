431a99d090128ebd43bc6cc0a357a12ea6405c69
Minchuk Ilya <cacatubutyt@gmail.com>
2023-10-28 19:09:52 +0200

    refactor: reject state from InfoSection component

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { AppState, Data } from '../App';
+import { AppState } from '../App';
 import './InfoSection.css';
 import { Card } from './Card';
 
@@ -7,41 +7,14 @@ interface PropsInterface {
   data: AppState;
 }
 
-interface StateInterface {
-  data: Data | null;
-}
-
-export class InfoSection extends Component<PropsInterface, StateInterface> {
-  state = {
-    data: this.props.data.data,
-  };
-
-  componentDidUpdate(
-    prevProps: Readonly<PropsInterface>,
-    prevState: Readonly<StateInterface>
-  ): void {
-    const currentPropsIds = this.props.data.data?.results.map(
-      (item) => item.id
-    );
-    const prevPropsIds = prevProps.data.data?.results.map((item) => item.id);
-    const currenStateIds = this.state.data?.results.map((item) => item.id);
-    const prevStateIds = prevState.data?.results.map((item) => item.id);
-    if (
-      currentPropsIds?.toString() !== prevPropsIds?.toString() ||
-      currenStateIds?.toString() !== prevStateIds?.toString()
-    ) {
-      this.setState({
-        data: this.props.data.data,
-      });
-    }
-  }
-
+export class InfoSection extends Component<PropsInterface> {
   render() {
+    const { data } = this.props;
     return (
       <section className="cards">
-        {this.props.data.isLoaded ? (
-          this.state.data?.results?.length ? (
-            this.state.data?.results.map((item) => (
+        {data.isLoaded ? (
+          data.data?.results?.length ? (
+            data.data?.results.map((item) => (
               <Card key={item.id} itemData={item} />
             ))
           ) : (
