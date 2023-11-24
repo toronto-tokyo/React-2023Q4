975ade2f90db33912570e9425dd5e11b2ac51331
Minchuk Ilya <cacatubutyt@gmail.com>
2023-11-24 11:50:51 +0100

    chore: setup next app

--- src/hooks/getCurrentPage.tsx
+++
@@ -1,7 +0,0 @@
-import { useSearchParams } from 'react-router-dom';
-
-export const useGetCurrentPage = () => {
-  const [searchParams] = useSearchParams();
-  const pageNumber = Number(searchParams.get('page'));
-  return pageNumber;
-};
